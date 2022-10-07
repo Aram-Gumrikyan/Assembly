154646048d6021ae1ab208bc4b6ceacce18f4aaa
Aram <aram.gumrikyan.01@gmail.com>
2022-10-07 09:54:50 +0400

    TUM-1: add mirror reverse functionality

--- index.cpp
+++ index.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-  int numbers[] = {7, 0, -4, 6, 5};
-  const int numbersLength = sizeof(numbers) / sizeof(numbers[0]);
+  short numbers[] = {72, 82, 42, 62, 52, 10};
+  const short numbersLength = sizeof(numbers) / sizeof(numbers[0]);
 
-  int minNumber = numbers[0];
+  short minNumber = numbers[0];
 
   for (int i = 1; i < numbersLength; i++)
   {
@@ -16,6 +16,22 @@ int main()
     }
   }
 
-  cout << "Bit changed minNumber is: " << ~minNumber << endl;
+  /**
+   *
+   * we are reversing index of values, not some values;
+   * reverseBits, mirror reverse consider to center of number bits
+   *
+   * */
+  short reversedMinNumber = 0;
+  short bitsCount = sizeof(minNumber) * 8;
+  for (int i = 0; i < bitsCount; i++)
+  {
+    short reversibleBite = (minNumber >> i) & 1;
+    short reversedBite = reversibleBite << (bitsCount - i - 1);
+    reversedMinNumber |= reversedBite;
+  }
+
+  cout << "minNumber is: " << minNumber << endl;
+  cout << "Bit reversed minNumber is: " << reversedMinNumber << endl;
   return 0;
 }
